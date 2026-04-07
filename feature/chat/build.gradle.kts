6fe2fa93672733e224baa4a1d3e3ea327ff879e4
robfox <jymbojymbolaya@hotmail.com>
2026-04-07 23:58:34 +0200

    Dependencies Added

--- feature/chat/build.gradle.kts
+++ feature/chat/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlinx.serialization)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.ksp)
+    alias(libs.plugins.google.dagger.hilt)
 }
 
 android {
@@ -27,16 +31,35 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
+
+    kotlin.compilerOptions.freeCompilerArgs =
+        listOf("-XXLanguage:+PropertyParamAnnotationDefaultTargetMode")
 }
 
 dependencies {
+    implementation(project(":common:framework"))
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.material3)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.runtime)
     implementation(libs.material)
+    implementation(libs.coil.io)
+    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.bundles.ktor)
+    implementation(libs.bundles.dagger.hilt)
+    ksp(libs.google.dagger.hilt.compiler)
 }
