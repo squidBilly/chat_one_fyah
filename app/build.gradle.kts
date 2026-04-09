58329f156c37cb6b8e41a67785e09e4017f5dbc2
robfox <jymbojymbolaya@hotmail.com>
2026-04-09 12:10:34 +0200

    Added Dependency Injection dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.ksp)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 android {
@@ -31,8 +33,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures {
         compose = true
@@ -44,6 +46,16 @@ dependencies {
     implementation(project(":feature:chat"))
     implementation(project(":feature:conversation"))
     implementation(project(":feature:createchat"))
+
+    implementation(libs.google.accompanist)
+    implementation(libs.google.dagger.hilt)
+    implementation(libs.json.serialization)
+    ksp(libs.google.dagger.hilt.compiler)
+    implementation(libs.hilt.navigation.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.bundles.ktor)
+    testImplementation(libs.kotlinx.coroutines.test)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -52,6 +64,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation.compose.plain)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/snowyfox/chatonefiyah/ui/screens/Routes.kt
@@ -0,0 +1,49 @@
+package com.snowyfox.chatonefiyah.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navDeepLink
+import androidx.navigation.toRoute
+import kotlinx.serialization.Serializable
+
+@Serializable
+interface Routes {
+    @Serializable
+    data object StartScreen : Routes
+
+    @Serializable
+    data class ChatScreen(val chatId: String) : Routes
+
+    @Serializable
+    data object ConversationList : Routes
+
+    @Serializable
+    data object Calls : Routes
+}
+
+@Composable
+fun FiyahChatNavigation(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = Routes.ConversationList) {
+
+    }
+}
+
+fun NavGraphBuilder.addConversationList(navController: NavController) {
+    composable<Routes.ConversationList> {
+
+    }
+}
+fun NavGraphBuilder.chatScreen(){
+    composable<Routes.ChatScreen>(
+        deepLinks = listOf(
+            navDeepLink<Routes.ChatScreen>(basePath = "https://whatspack.com/chat?chatId={chatId}")
+        )
+    ){
+        val chatId = it.toRoute<Routes.ChatScreen>().chatId
+
+    }
+}
